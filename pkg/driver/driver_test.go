f7f5222d6c0d7c2dab42e42da7e56f8ec6ad33e4
Matt Laidlaw <mlaidlaw01@gmail.com>
2021-08-29 19:41:01 -0700

    test client

--- pkg/driver/driver_test.go
+++ pkg/driver/driver_test.go
@@ -5,6 +5,23 @@ import (
 	"testing"
 )
 
-func TestClient_Del(t *testing.T) {
+func TestClient(t *testing.T) {
+	client, err := NewClient("localhost:6342")
+	assert.ExpectEq(err, nil, t)
 
+	insertedCount, err := client.Set("key", "val")
+	assert.ExpectEq(err, nil, t)
+	assert.ExpectEq(insertedCount, 1, t)
+
+	value, err := client.Get("key")
+	assert.ExpectEq(err, nil, t)
+	assert.ExpectEq(value, "val", t)
+
+	deletedCount, err := client.Del("key")
+	assert.ExpectEq(err, nil, t)
+	assert.ExpectEq(deletedCount, 1, t)
+
+	value, err = client.Get("key")
+	assert.ExpectEq(err, nil, t)
+	assert.ExpectEq(value, "", t)
 }
